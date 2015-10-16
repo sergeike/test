f39c90e627402b6176b6946fde641e28c7e67b52
sergelombaerts <serge.lombaerts@gmail.com>
2015-10-16 18:14:30 +0200

    use a central repo. push first changes. add Pojo change App

--- src/main/java/be/lombaer/test/App.java
+++ src/main/java/be/lombaer/test/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "2s" );
+        Pojo aPojo = new Pojo();
+        System.out.println( aPojo.getaString() );
     }
 }
---
+++ src/main/java/be/lombaer/test/Pojo.java
@@ -0,0 +1,18 @@
+package be.lombaer.test;
+
+/**
+ * Created by sergelombaerts on 16/10/15.
+ */
+public class Pojo {
+    String aString = "default";
+
+    public String getaString() {
+        return aString;
+    }
+
+    public void setaString(String aString) {
+        this.aString = aString;
+    }
+
+
+}
