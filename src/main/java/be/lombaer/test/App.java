d446190064f28cf271a81fc914a6bfa09f9449f0
sergelombaerts <serge.lombaerts@gmail.com>
2015-10-16 20:46:11 +0200

    test connection to gitHub again

--- src/main/java/be/lombaer/test/App.java
+++ src/main/java/be/lombaer/test/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         Pojo aPojo = new Pojo();
-        aPojo.setaString("Hello World");
+        aPojo.setaString("Hello GitHub");
         System.out.println( aPojo.getaString() );
     }
 }
